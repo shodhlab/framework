b4e08f7b27ecf1b87718ef10aee34fd1b21b3971
InferqTejas <tejas@inferq.com>
2024-06-26 10:36:47 +0000

    added: top K, top P sampling with temperature and penalize if repeat N tokens

--- custom/inference.py
+++ custom/inference.py
@@ -52,22 +52,54 @@ class Inference:
         with torch.no_grad():  # Ensure no gradient computation
             op = self.model.predict_step(x)
         print(op.shape)
+        
+    def top_k_sampling(self,logits, k):
+        values, indices = torch.topk(logits, k)
+        probs = torch.softmax(values, dim=-1)
+        chosen_index = torch.multinomial(probs, 1)
+        return indices[chosen_index].item()
 
-    def infer(self, text, max_length):
+    def top_p_sampling(self, logits, p):
+        sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+        cumulative_probs = torch.cumsum(torch.softmax(sorted_logits, dim=-1), dim=-1)
+        sorted_indices_to_remove = cumulative_probs > p
+        sorted_indices_to_remove[:, 1:] = sorted_indices_to_remove[:, :-1].clone()
+        sorted_indices_to_remove[:, 0] = 0
+
+        indices_to_remove = sorted_indices_to_remove.scatter(1, sorted_indices, sorted_indices_to_remove)
+        logits[indices_to_remove] = -float('Inf')
+        probs = torch.softmax(logits, dim=-1)
+        chosen_index = torch.multinomial(probs, 1)
+        return chosen_index.item()
+    
+    def penalize_repeats(self, logits, token_ids, penalty, N):
+        recent_tokens = token_ids[-N:]
+        for token in set(recent_tokens):
+            logits[token] -= penalty
+        return logits
+
+    def infer(self, text, max_length, top_k=40, top_p=.5, temperature=0.7, penalty=1.18, N=5):
         x = self.tokenizer.encode(text, out_type=int)
         outputs = []
         for _ in range(max_length):
-            op = self.get_output(x)
+            logits = self.get_output(x)
+            logits = logits.squeeze()[-1]
 
-            # idx = torch.topk(op[:,-1,:],k=10,dim=-1,largest=True,sorted=True)[1]
-            # select = torch.randint(low=0,high=idx.shape[-1],size=(1,))
-            # op = int(idx[0][select].cpu().numpy()[0])
-            # print(op)
-            op = torch.argmax(op, dim=-1)
-            op = op.squeeze().tolist()[-1]
+            # Apply temperature scaling
+            logits = logits / temperature
+
+            # Penalize repeating tokens
+            logits = self.penalize_repeats(logits, x, penalty, N)
+
+            if top_k > 0:
+                op = self.top_k_sampling(logits, top_k)
+            elif top_p > 0.0:
+                op = self.top_p_sampling(logits, top_p)
+            else:
+                op = torch.argmax(torch.softmax(logits,dim=-1), dim=-1).item()
+
             x.append(op)
             outputs.append(op)
             if op == self.tokenizer.eos_id():
                 break
         return self.tokenizer.decode(outputs)
-
